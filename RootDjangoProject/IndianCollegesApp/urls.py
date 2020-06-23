516a8962356cd6a74ae0c21d35a7b76e98b42c34
NisithMondal <mondalnisith55@gmail.com>
2020-06-24 00:56:40 +0530

    new updates

--- RootDjangoProject/IndianCollegesApp/urls.py
+++ RootDjangoProject/IndianCollegesApp/urls.py
@@ -12,5 +12,22 @@ urlpatterns = [
     path('top-engneering-colleges/', views.topEngneeringColleges),
     path('top-medical-colleges/', views.topMedicalColleges),
     path('top-diploma-colleges/', views.topDiplomaColleges),
-    path('states/<int:state_id>/', views.state),
+
+    path('home/states/<int:state_id>/', views.homeCollegesStateWise),
+    path('home/city/<int:city_id>/', views.homeCollegesCityWise),
+    path('home/state/city/<int:city_id>/', views.homeCollegesStateCityWise),
+
+
+    path('engneering-colleges/states/<int:state_id>/', views.engneeringCollegesStateWise),
+    path('engneering-colleges/city/<int:city_id>/', views.engneeringCollegesCityWise),
+    path('engneering-colleges/state/city/<int:city_id>/', views.engneeringCollegesStateCityWise),
+
+    path('medical-colleges/states/<int:state_id>/', views.medicalCollegesStateWise),
+    path('medical-colleges/city/<int:city_id>/', views.medicalCollegesCityWise),
+    path('medical-colleges/state/city/<int:city_id>/', views.medicalCollegesStateCityWise),
+
+    path('diploma-colleges/states/<int:state_id>/', views.diplomaCollegesStateWise),
+    path('diploma-colleges/city/<int:city_id>/', views.diplomaCollegesCityWise),
+    path('diploma-colleges/state/city/<int:city_id>/', views.diplomaCollegesStateCityWise),
+
 ]
--- RootDjangoProject/IndianCollegesApp/views.py
+++ RootDjangoProject/IndianCollegesApp/views.py
@@ -11,11 +11,11 @@ def home(request):
     firstState = allStatesListForDropdown.pop(0)
     otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
                         allStatesListForDropdown.pop(0)]
-    isRequestFromHome = True
+    isStateNotSelected = True
     return render(request, 'IndianCollegesApp/home.html', {'firstState': firstState, 'allCitiesList': allCitiesList,
                                                            'allStatesListForDropdown': allStatesListForDropdown,
                                                            'otherThreeStates': otherThreeStates,
-                                                           'isRequestFromHome': isRequestFromHome})
+                                                           'isStateNotSelected': isStateNotSelected})
 
 
 def engneeringColleges(request):
@@ -24,12 +24,12 @@ def engneeringColleges(request):
     firstState = allStatesListForDropdown.pop(0)
     otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
                         allStatesListForDropdown.pop(0)]
-    isRequestFromHome = True
+    isStateNotSelected = True
     return render(request, 'IndianCollegesApp/engneering_colleges.html',
                   {'firstState': firstState, 'allCitiesList': allCitiesList,
                    'allStatesListForDropdown': allStatesListForDropdown,
                    'otherThreeStates': otherThreeStates,
-                   'isRequestFromHome': isRequestFromHome})
+                   'isStateNotSelected': isStateNotSelected})
 
 
 def medicalColleges(request):
@@ -38,12 +38,12 @@ def medicalColleges(request):
     firstState = allStatesListForDropdown.pop(0)
     otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
                         allStatesListForDropdown.pop(0)]
-    isRequestFromHome = True
+    isStateNotSelected = True
     return render(request, 'IndianCollegesApp/medical_colleges.html',
                   {'firstState': firstState, 'allCitiesList': allCitiesList,
                    'allStatesListForDropdown': allStatesListForDropdown,
                    'otherThreeStates': otherThreeStates,
-                   'isRequestFromHome': isRequestFromHome})
+                   'isStateNotSelected': isStateNotSelected})
 
 
 def diplomaColleges(request):
@@ -52,12 +52,12 @@ def diplomaColleges(request):
     firstState = allStatesListForDropdown.pop(0)
     otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
                         allStatesListForDropdown.pop(0)]
-    isRequestFromHome = True
+    isStateNotSelected = True
     return render(request, 'IndianCollegesApp/diploma_colleges.html',
                   {'firstState': firstState, 'allCitiesList': allCitiesList,
                    'allStatesListForDropdown': allStatesListForDropdown,
                    'otherThreeStates': otherThreeStates,
-                   'isRequestFromHome': isRequestFromHome})
+                   'isStateNotSelected': isStateNotSelected})
 
 
 def topIIT(request):
@@ -76,15 +76,181 @@ def topDiplomaColleges(request):
     return HttpResponse("Top Diploma Colleges")
 
 
-def state(request, state_id):
+def homeCollegesStateWise(request, state_id):
     allStatesListForDropdown = list(AllStates.objects.exclude(id=state_id))
     selectedState = AllStates.objects.filter(id=state_id)
     allCitiesList = list(AllCities.objects.filter(state_id=state_id))
     selectedState = selectedState[0]
     otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
                         allStatesListForDropdown.pop(0)]
-    isRequestFromHome = False
+    isStateNotSelected = False
+    state = "/state"
     return render(request, 'IndianCollegesApp/home.html',
                   {'selectedState': selectedState, 'allCitiesList': allCitiesList,
                    'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
-                   'isRequestFromHome': isRequestFromHome})
+                   'isStateNotSelected': isStateNotSelected, 'state': state})
+
+
+def homeCollegesCityWise(request, city_id):
+    allStatesListForDropdown = list(AllStates.objects.all())
+    allCitiesList = list(AllCities.objects.all())
+    firstState = allStatesListForDropdown.pop(0)
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = True
+    return render(request, 'IndianCollegesApp/home.html',
+                  {'firstState': firstState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'city_id': city_id})
+
+
+def homeCollegesStateCityWise(request, city_id):
+    obj = AllCities.objects.get(id=city_id)
+    state_id = obj.state_id_id
+    allStatesListForDropdown = list(AllStates.objects.exclude(id=state_id))
+    selectedState = AllStates.objects.filter(id=state_id)
+    allCitiesList = list(AllCities.objects.filter(state_id=state_id))
+    selectedState = selectedState[0]
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = False
+    state = "/state"
+    return render(request, 'IndianCollegesApp/home.html',
+                  {'selectedState': selectedState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'city_id': city_id, 'state': state})
+
+
+def engneeringCollegesStateWise(request, state_id):
+    allStatesListForDropdown = list(AllStates.objects.exclude(id=state_id))
+    selectedState = AllStates.objects.filter(id=state_id)
+    allCitiesList = list(AllCities.objects.filter(state_id=state_id))
+    selectedState = selectedState[0]
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = False
+    state = "/state"
+    return render(request, 'IndianCollegesApp/engneering_colleges.html',
+                  {'selectedState': selectedState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'state': state})
+
+
+def engneeringCollegesCityWise(request, city_id):
+    allStatesListForDropdown = list(AllStates.objects.all())
+    allCitiesList = list(AllCities.objects.all())
+    firstState = allStatesListForDropdown.pop(0)
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = True
+    return render(request, 'IndianCollegesApp/engneering_colleges.html',
+                  {'firstState': firstState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'city_id': city_id})
+
+
+def engneeringCollegesStateCityWise(request, city_id):
+    obj = AllCities.objects.get(id=city_id)
+    state_id = obj.state_id_id
+    allStatesListForDropdown = list(AllStates.objects.exclude(id=state_id))
+    selectedState = AllStates.objects.filter(id=state_id)
+    allCitiesList = list(AllCities.objects.filter(state_id=state_id))
+    selectedState = selectedState[0]
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = False
+    state = "/state"
+    return render(request, 'IndianCollegesApp/engneering_colleges.html',
+                  {'selectedState': selectedState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'city_id': city_id, 'state': state})
+
+
+def medicalCollegesStateWise(request, state_id):
+    allStatesListForDropdown = list(AllStates.objects.exclude(id=state_id))
+    selectedState = AllStates.objects.filter(id=state_id)
+    allCitiesList = list(AllCities.objects.filter(state_id=state_id))
+    selectedState = selectedState[0]
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = False
+    state = "/state"
+    return render(request, 'IndianCollegesApp/medical_colleges.html',
+                  {'selectedState': selectedState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'state': state})
+
+
+def medicalCollegesCityWise(request, city_id):
+    allStatesListForDropdown = list(AllStates.objects.all())
+    allCitiesList = list(AllCities.objects.all())
+    firstState = allStatesListForDropdown.pop(0)
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = True
+    return render(request, 'IndianCollegesApp/medical_colleges.html',
+                  {'firstState': firstState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'city_id': city_id})
+
+
+def medicalCollegesStateCityWise(request, city_id):
+    obj = AllCities.objects.get(id=city_id)
+    state_id = obj.state_id_id
+    allStatesListForDropdown = list(AllStates.objects.exclude(id=state_id))
+    selectedState = AllStates.objects.filter(id=state_id)
+    allCitiesList = list(AllCities.objects.filter(state_id=state_id))
+    selectedState = selectedState[0]
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = False
+    state = "/state"
+    return render(request, 'IndianCollegesApp/medical_colleges.html',
+                  {'selectedState': selectedState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'city_id': city_id, 'state': state})
+
+
+def diplomaCollegesStateWise(request, state_id):
+    allStatesListForDropdown = list(AllStates.objects.exclude(id=state_id))
+    selectedState = AllStates.objects.filter(id=state_id)
+    allCitiesList = list(AllCities.objects.filter(state_id=state_id))
+    selectedState = selectedState[0]
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = False
+    state = "/state"
+    return render(request, 'IndianCollegesApp/diploma_colleges.html',
+                  {'selectedState': selectedState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'state': state})
+
+
+def diplomaCollegesCityWise(request, city_id):
+    allStatesListForDropdown = list(AllStates.objects.all())
+    allCitiesList = list(AllCities.objects.all())
+    firstState = allStatesListForDropdown.pop(0)
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = True
+    return render(request, 'IndianCollegesApp/diploma_colleges.html',
+                  {'firstState': firstState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'city_id': city_id})
+
+
+def diplomaCollegesStateCityWise(request, city_id):
+    obj = AllCities.objects.get(id=city_id)
+    state_id = obj.state_id_id
+    allStatesListForDropdown = list(AllStates.objects.exclude(id=state_id))
+    selectedState = AllStates.objects.filter(id=state_id)
+    allCitiesList = list(AllCities.objects.filter(state_id=state_id))
+    selectedState = selectedState[0]
+    otherThreeStates = [allStatesListForDropdown.pop(0), allStatesListForDropdown.pop(0),
+                        allStatesListForDropdown.pop(0)]
+    isStateNotSelected = False
+    state = "/state"
+    return render(request, 'IndianCollegesApp/diploma_colleges.html',
+                  {'selectedState': selectedState, 'allCitiesList': allCitiesList,
+                   'allStatesListForDropdown': allStatesListForDropdown, 'otherThreeStates': otherThreeStates,
+                   'isStateNotSelected': isStateNotSelected, 'city_id': city_id, 'state': state})
